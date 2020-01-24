51c49df89bc422259ee672b44609526c4257ba9c
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-24 14:17:33 +0100

    :fire: add content in function autobind

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,17 @@ function autobind(
   target: any,
   methodName: string,
   descriptor: PropertyDescriptor
-) {}
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    }
+  };
+  return adjDescriptor;
+}
 
 // ProjectInput Class
 class ProjectInput {
@@ -42,6 +52,7 @@ class ProjectInput {
     this.attach();
   }
 
+  @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
     console.log(this.titleInputElement.value);
