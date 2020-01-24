78975a4924df3be872e883a12b6ef8f90b58d31d
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-24 14:56:31 +0100

    :zap: :rocket: add if method to see if it good validatable input value

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,20 @@ function validate(validatableInput: Validatable) {
   if (validatableInput.required) {
     isValid = isValid && validatableInput.value.toString().trim().length !== 0;
   }
+  if (
+    validatableInput.minLength != null &&
+    typeof validatableInput.value == "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length > validatableInput.minLength;
+  }
+  if (
+    validatableInput.maxLength != null &&
+    typeof validatableInput.value == "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length < validatableInput.maxLength;
+  }
   return isValid;
 }
 
