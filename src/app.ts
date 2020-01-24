5922b58887c3c2496b8152aa9fe79b2a5fc5caed
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-24 14:59:01 +0100

    :fire: add new verif

--- src/app.ts
+++ src/app.ts
@@ -29,6 +29,18 @@ function validate(validatableInput: Validatable) {
     isValid =
       isValid && validatableInput.value.length < validatableInput.maxLength;
   }
+  if (
+    validatableInput.min != null &&
+    typeof validatableInput.value == "number"
+  ) {
+    isValid = isValid && validatableInput.value > validatableInput.min;
+  }
+  if (
+    validatableInput.max != null &&
+    typeof validatableInput.value == "number"
+  ) {
+    isValid = isValid && validatableInput.value < validatableInput.max;
+  }
   return isValid;
 }
 
