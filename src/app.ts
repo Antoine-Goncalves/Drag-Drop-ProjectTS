64d3c72897bc700757158c78ffca021c1e088e82
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-24 16:37:43 +0100

    :zap: add 2 other abstract keyword

--- src/app.ts
+++ src/app.ts
@@ -145,6 +145,9 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
       this.element
     );
   }
+
+  abstract configure(): void;
+  abstract renderContent(): void;
 }
 
 // ProjectList Class
