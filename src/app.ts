c1c405f80d45c631e045356bfd5b73bc5df949a4
Antoine Goncalves <goncalves.devweb@gmail.com>
2020-01-24 15:14:14 +0100

    :fire: :zap:  add content in constructor private

--- src/app.ts
+++ src/app.ts
@@ -60,7 +60,25 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
 
 // ProjectList Class
 
-class ProjectList {}
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: "active" | "finished") {
+    this.templateElement = document.getElementById(
+      "project-list"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = "user-input";
+  }
+}
 
 // ProjectInput Class
 class ProjectInput {
